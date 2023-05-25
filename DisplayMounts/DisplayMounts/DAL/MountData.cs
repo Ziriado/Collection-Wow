6199e8135c21a5e0053d7147e2e07c78f68a917d
Ziriado <115992429+Ziriado@users.noreply.github.com>
2023-05-25 11:10:48 +0200

    more start of admin

--- DisplayMounts/DisplayMounts/DAL/MountData.cs
+++ DisplayMounts/DisplayMounts/DAL/MountData.cs
@@ -20,5 +20,58 @@ namespace DisplayMounts.DAL
                 return mounts;
             }
         }
+
+        public static async Task<Models.Mounts> GetMount(int id)
+        {
+            Models.Mounts mount = new();
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = BaseAdress;
+                HttpResponseMessage response = await client.GetAsync("api/Mount/"+id);
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseString = await response.Content.ReadAsStringAsync();
+                    mount = JsonSerializer.Deserialize<Models.Mounts>(responseString);
+                }
+                return mount;
+            }
+        }
+        public static async Task DeleteMount(int id)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = BaseAdress;
+                HttpResponseMessage response = await client.DeleteAsync("api/Mount/" + id);
+            }
+        }
+        public static async Task SaveMount(Models.Mounts mount)
+        {
+            var onemount = (await GetMounts()).Where(p => p.ID == mount.ID).FirstOrDefault();
+
+            if (onemount != null)
+            {
+                using (var client = new HttpClient())
+                {
+                    client.BaseAddress = BaseAdress;
+                    var json = JsonSerializer.Serialize(mount);
+                    StringContent httpContent = new StringContent(json, System.Text.Encoding.UTF8, "application/json");
+                    HttpResponseMessage response = await client.PutAsync("api/Products/" + mount.ID, httpContent);
+
+                }
+            }
+            else
+            {
+                using (var client = new HttpClient())
+                {
+                    client.BaseAddress = BaseAdress;
+
+                    var json = JsonSerializer.Serialize(mount);
+                    StringContent httpContent = new StringContent(json, System.Text.Encoding.UTF8, "application/json");
+                    HttpResponseMessage response = await client.PostAsync("api/Mount/", httpContent);
+
+                }
+            }
+        }
     }
 }
--- DisplayMounts/DisplayMounts/Pages/AdminPage.cshtml.cs
+++ DisplayMounts/DisplayMounts/Pages/AdminPage.cshtml.cs
@@ -5,15 +5,38 @@ namespace DisplayMounts.Pages
 {
     public class PrivacyModel : PageModel
     {
-        private readonly ILogger<PrivacyModel> _logger;
+        [BindProperty]
+        public Models.Mounts NewMount { get; set; }
+        public List<Models.Mounts> Mounts { get; set; }
+        [BindProperty]
+        public string FactionName { get; set; }
 
-        public PrivacyModel(ILogger<PrivacyModel> logger)
+        public async Task<IActionResult>OnGetAsync(int editid,int deleteindex)
         {
-            _logger = logger;
+            Mounts = await DAL.MountData.GetMounts();
+
+            if (editid != 0)
+            {
+                NewMount = await DAL.MountData.GetMount(editid);
+            }
+
+            if (deleteindex != 0)
+            {
+                await DAL.MountData.DeleteMount(deleteindex - 1);
+                Mounts = await DAL.MountData.GetMounts();
+            }
+
+            return Page();
         }
-
-        public void OnGet()
+        public async Task<IActionResult> OnPostAsync()
         {
+            if (ModelState.IsValid)
+            {
+                await DAL.MountData.SaveMount(NewMount);
+            }
+            Mounts = await DAL.MountData.GetMounts();
+            return Page();
+
         }
     }
 }
