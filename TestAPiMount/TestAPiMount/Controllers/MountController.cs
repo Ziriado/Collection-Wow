e1447567f2bfbfb1826668439efea4256cc8a92f
Ziriado <115992429+Ziriado@users.noreply.github.com>
2023-05-25 10:39:14 +0200

    start of api finish

--- TestAPiMount/TestAPiMount/Controllers/MountController.cs
+++ TestAPiMount/TestAPiMount/Controllers/MountController.cs
@@ -12,5 +12,27 @@ namespace TestAPiMount.Controllers
         {
             return Models.Mount.GetMounts();
         }
+        [HttpGet("{id}")]
+        public async Task<Models.Mount>Get(int id)
+        {
+            var mount = await DAL.MountManager.GetOneMount(id);
+
+            return mount;
+        }
+        [HttpPost]
+        public async Task Post([FromBody]Models.Mount mount)
+        {
+            await DAL.MountManager.CreateMount(mount);
+        }
+        [HttpPut("{id}")]
+        public async Task Put(int id, [FromBody]Models.Mount mount)
+        {
+            await DAL.MountManager.UpdateMount(mount, id);
+        }
+        [HttpDelete("{id}")]
+        public async Task Delete(int id)
+        {
+            await DAL.MountManager.DeleteMount(id);
+        }
     }
 }
--- TestAPiMount/TestAPiMount/DAL/MountManager.cs
+++ TestAPiMount/TestAPiMount/DAL/MountManager.cs
@@ -5,12 +5,63 @@
         public static List<Models.Mount>Mounts { get; set; }
 
 
-        //public static async Task<List<Models.Mount>> GetAllMounts()
-        //{
-        //    if(Mounts == null || !Mounts.Any())
-        //    {
-        //        Mounts = await
-        //    }
-        //}
+        public static async Task<Models.Mount>GetOneMount(int id)
+        {
+            if (Mounts == null || !Mounts.Any())
+            {
+                Mounts =  Models.Mount.GetMounts();
+            }
+
+            var existingProd = Mounts.Where(p => p.ID == id).SingleOrDefault();
+
+            if (existingProd != null)
+            {
+                return existingProd;
+            }
+            else
+            {
+                return null;
+            }
+        }
+        public static async Task CreateMount(Models.Mount mount)
+        {
+            if (Mounts is null)
+            {
+                Mounts = Models.Mount.GetMounts();
+            }
+            var tempId = Mounts.LastOrDefault();
+            mount.ID = tempId.ID + 1;
+            Mounts.Add(mount);
+
+        }
+        public static async Task UpdateMount(Models.Mount mount, int id)
+        {
+            if (Mounts is null)
+            {
+                Mounts = Models.Mount.GetMounts();
+            }
+
+            var existingMount = Mounts.Where(p => p.ID == id).SingleOrDefault();
+
+            if (existingMount != null)
+            {
+                existingMount.WebLink = mount.WebLink;
+                existingMount.ImgLink = mount.ImgLink;
+                existingMount.Expansion = mount.Expansion;
+                existingMount.Class = mount.Class;
+                existingMount.Faction = mount.Faction;
+                existingMount.Name = mount.Name;
+            }
+
+        }
+        public static async Task DeleteMount(int id)
+        {
+            if (Mounts is null)
+            {
+                Mounts = Models.Mount.GetMounts();
+            }
+
+            Mounts.RemoveAt(id);
+        }
     }
 }
