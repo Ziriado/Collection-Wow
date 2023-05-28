963d310bdc37a413069740424a8b97f8f2ad5d02
Extender <dann_137@hotmail.com>
2023-05-28 02:15:29 +0200

    Removed unnecessary Code and made methods better

--- DisplayMounts/DisplayMounts/Pages/Index.cshtml.cs
+++ DisplayMounts/DisplayMounts/Pages/Index.cshtml.cs
@@ -15,13 +15,22 @@ namespace DisplayMounts.Pages
         private readonly Data.DisplayMountsContext _context;
 
         public DisplayMountsUser MyUser { get; set; }
+        public static List<Models.Mounts> Mounts { get; set; }
+        public List<Models.MountCollected> MountsCollected { get; set; }
+        public List<Models.Mounts> ShowMounts { get; set; }
+        public Models.Mounts OneMount { get; set; }
+        public List<Models.Comments> Comments { get; set; }
+        [BindProperty]
+        public Models.Comments Comment { get; set; }
+        [BindProperty]
+        public PreferencesModel Preferences { get; set; }
+
         public IndexModel(UserManager<DisplayMountsUser> userManager, Data.DisplayMountsContext context)
         {
-            Mounts = new List<Models.Mounts>();
-            ShowMounts = new List<Models.Mounts>();
             _userManger = userManager;
             _context = context;
-
+            Mounts = new List<Models.Mounts>();
+            ShowMounts = new List<Models.Mounts>();
             Preferences = new PreferencesModel
             {
                 Factions = new List<string>(),
@@ -31,157 +40,30 @@ namespace DisplayMounts.Pages
             };
         }
 
-        [BindProperty]
-        public PreferencesModel Preferences { get; set; }
-
-        public static List<Models.Mounts> Mounts { get; set; }
-        public List<Models.MountCollected> MountsCollected { get; set; }
-        [BindProperty]
-        public Models.MountCollected MountCollectedOne { get; set; }
-        public List<Models.Mounts> ShowMounts { get; set; }
-        public Models.Mounts OneMount { get; set; }
-        public static int TempId { get; set; }
-        public bool HasMount { get; set; }
-        public List<Models.Comments> Comments { get; set; }
-        [BindProperty]
-        public Models.Comments Comment { get; set; }
-
         public async Task OnPost()
         {
-            Mounts = await DAL.MountData.GetMounts();
-
-            await ShowUserPreference();
-
-
+            await SetUserPreference();
         }
-
-        public async Task ShowUserPreference()
-        {
-            List<string> factionlist = new List<string>();
-            List<string> expansionlist = new List<string>();
-            List<string> playerClassesList = new List<string>();
-
-            if (!User.Identity.IsAuthenticated)
-            {
-                if (Preferences.Factions == null) factionlist.Add("Neutral");
-                else factionlist = Preferences.Factions;
-
-                if (Preferences.Expansions == null) expansionlist.Add("Vanilla");
-                else expansionlist = Preferences.Expansions;
-
-                if (Preferences.Classes == null) playerClassesList.Add("Neutral");
-                else playerClassesList = Preferences.Classes;
-            }
-            else
-            {
-                MyUser = await _userManger.GetUserAsync(User);
-                MountsCollected = await _context.Collected.Where(x => x.UserId == MyUser.Id).ToListAsync();
-
-                if (Preferences.Factions == null) 
-                {
-                    factionlist.Add("Neutral");
-                    factionlist.Add(MyUser.Faction);
-                }
-                else factionlist = Preferences.Factions;
-
-                if (Preferences.Expansions == null)
-                {
-                    expansionlist.Add("Vanilla");
-                    expansionlist.Add("Burning Crusade");
-                    expansionlist.Add("Wrath of the Lich King");
-                }
-                else expansionlist = Preferences.Expansions;
-
-                if (Preferences.Classes == null)
-                {
-                    playerClassesList.Add("Neutral");
-                    playerClassesList.Add(MyUser.Class);
-                }
-                else playerClassesList = Preferences.Classes;
-            }
-
-            ShowMounts = factionlist.SelectMany(faction =>
-                            playerClassesList.SelectMany(playerClass =>
-                                expansionlist.SelectMany(expansion =>
-                                    Mounts.Where(m =>
-                                        m.Faction == faction &&
-                                        m.PlayerClass == playerClass &&
-                                        m.Expansion == expansion)))).ToList();
-
-            if (Preferences.Collected != null && Preferences.Collected.Count == 1)
-            {
-                MyUser = await _userManger.GetUserAsync(User);
-                var dbMounts = await _context.Collected
-                                        .Where(x => x.UserId == MyUser.Id)
-                                        .Select(x => x.MountId)
-                                        .ToListAsync();
-
-                if (Preferences.Collected != null && Preferences.Collected[0] == "Collected")
-                {
-                    ShowMounts = ShowMounts.Where(m => dbMounts.Contains(m.Id)).ToList();
-                }
-                else if (Preferences.Collected != null && Preferences.Collected[0] == "NotCollected")
-                {
-                    ShowMounts = ShowMounts.Where(m => !dbMounts.Contains(m.Id)).ToList();
-                }
-            }
-        }
-
-
-        public async Task<IActionResult> OnGetAsync(int id, string moreInfo)
-        {
-            Mounts = await DAL.MountData.GetMounts();
-            ShowMounts = await DAL.MountData.GetMounts();
-            MyUser = await _userManger.GetUserAsync(User);
-            if (User.Identity.IsAuthenticated && MyUser != null)
-                MountsCollected = await _context.Collected.Where(x => x.UserId == MyUser.Id).ToListAsync();
-
-            if (id != 0)
-            {
-                Comments = await _context.Comment.Where(c => c.MountId == id).ToListAsync();
-
-                OneMount = await DAL.MountDataOne.GetOne(Mounts, id);
-                TempId = id;
-                if (User.Identity.IsAuthenticated)
-                {
-                    var dbMount = await _context.Collected.Where(x => x.UserId == MyUser.Id && x.MountId == id).ToListAsync();
-
-                    if (dbMount.Count != 0)
-                    {
-                        HasMount = true;
-                    }
-                }
-
-            }
-            if (moreInfo != null)
-            {
-                Response.Redirect(moreInfo);
-            }
-
-            return Page();
-        }
-
-
-        public async Task<IActionResult> OnPostSaveMount([FromForm] int mountId)
+        public async Task<IActionResult> OnPostToggleMount([FromForm] int mountId)
         {
             MyUser = await _userManger.GetUserAsync(User);
-            MountCollectedOne.MountId = mountId;
-            MountCollectedOne.UserId = MyUser.Id;
-            var dbMount = await _context.Collected.Where(x => x.UserId == MyUser.Id && x.MountId == mountId).ToListAsync();
+            var dbMountCollected = await _context.Collected
+                .Where(x => x.UserId == MyUser.Id && x.MountId == mountId)
+                .FirstOrDefaultAsync();
 
-            if (dbMount.Count == 0)
-                _context.Add(MountCollectedOne);
+            if (dbMountCollected == null)
+                _context.Add(new MountCollected { UserId = MyUser.Id, MountId = mountId });
             else
-                _context.Remove(dbMount[0]);
+                _context.Remove(dbMountCollected);
 
             await _context.SaveChangesAsync();
+
             return RedirectToPage("./Index");
         }
-
         public async Task<IActionResult> OnPostPostComment([FromForm] int mountId)
         {
             MyUser = await _userManger.GetUserAsync(User);
-            Comment.CommentCreated=DateTime.Now;
+            Comment.CommentCreated = DateTime.Now;
             Comment.UserId = MyUser.Id;
             Comment.UserName = MyUser.CharacterName;
             Comment.MountId = mountId;
@@ -189,7 +71,6 @@ namespace DisplayMounts.Pages
             await _context.SaveChangesAsync();
             return Page();
         }
-
         public async Task<IActionResult> OnPostRemoveComment([FromForm] int commentId)
         {
             var dbComments = await _context.Comment.Where(c => c.Id == commentId).FirstOrDefaultAsync();
@@ -201,6 +82,74 @@ namespace DisplayMounts.Pages
             }
 
             return Page();
+        }
+
+        public async Task<IActionResult> OnGetAsync(int id, string moreInfo)
+        {
+            Mounts = await DAL.MountData.GetMounts();
+            ShowMounts = await DAL.MountData.GetMounts();
+            MyUser = await _userManger.GetUserAsync(User);
+
+            if (User.Identity.IsAuthenticated && MyUser != null)
+                MountsCollected = await _context.Collected.Where(x => x.UserId == MyUser.Id).ToListAsync();
+
+            if (id != 0)
+            {
+                Comments = await _context.Comment.Where(c => c.MountId == id).ToListAsync();
+                OneMount = Mounts.Where(x => x.Id == id).FirstOrDefault();
+            }
+
+            if (moreInfo != null)
+            {
+                Response.Redirect(moreInfo);
+            }
+
+            return Page();
+        }
+
+        public async Task SetUserPreference()
+        {
+            Mounts = await DAL.MountData.GetMounts();
+
+            List<string> factionList = Preferences.Factions ?? new List<string> { "Neutral" };
+            List<string> expansionList = Preferences.Expansions ?? new List<string> { "Vanilla", "Burning Crusade", "Wrath of the Lich King" };
+            List<string> playerClassesList = Preferences.Classes ?? new List<string> { "Neutral" };
+
+            if (User.Identity.IsAuthenticated)
+            {
+                MyUser = await _userManger.GetUserAsync(User);
+                MountsCollected = await _context.Collected.Where(x => x.UserId == MyUser.Id).ToListAsync();
+
+                if (Preferences.Factions == null) 
+                    factionList.Add(MyUser.Faction);
+
+                if (Preferences.Classes == null)
+                    playerClassesList.Add(MyUser.Class);
+            }
+
+            ShowMounts = factionList
+                .SelectMany(faction => playerClassesList
+                    .SelectMany(playerClass => expansionList
+                        .SelectMany(expansion => Mounts
+                            .Where(m =>
+                                m.Faction == faction &&
+                                m.PlayerClass == playerClass &&
+                                m.Expansion == expansion))))
+                .ToList();
+
+            if (Preferences.Collected != null && Preferences.Collected.Count == 1)
+            {
+                MyUser = await _userManger.GetUserAsync(User);
+                var collectedMountId = await _context.Collected
+                                        .Where(x => x.UserId == MyUser.Id)
+                                        .Select(x => x.MountId)
+                                        .ToListAsync();
+
+                if (Preferences.Collected[0] == "Collected")
+                    ShowMounts = ShowMounts.Where(m => collectedMountId.Contains(m.Id)).ToList();
+                else if (Preferences.Collected[0] == "NotCollected")
+                    ShowMounts = ShowMounts.Where(m => !collectedMountId.Contains(m.Id)).ToList();
+            }
         }
     }
 }
