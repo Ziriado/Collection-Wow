ec6ae8c7bc7572c896728bbcf86fd799ef241628
Ziriado <115992429+Ziriado@users.noreply.github.com>
2023-05-29 15:23:00 +0200

    correct db connected and some more changes

--- DisplayMounts/DisplayMounts/Pages/AdminPage.cshtml.cs
+++ DisplayMounts/DisplayMounts/Pages/AdminPage.cshtml.cs
@@ -1,17 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DisplayMounts.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace DisplayMounts.Pages
 {
     public class PrivacyModel : PageModel
     {
+        private readonly UserManager<DisplayMountsUser> _userManger;
         [BindProperty]
         public Models.Mounts NewMount { get; set; }
         public List<Models.Mounts> Mounts { get; set; }
+        public DisplayMountsUser MyUser { get; set; }
+        public PrivacyModel(UserManager<DisplayMountsUser> userManager)
+        {
+            _userManger = userManager; 
+        }
 
         public async Task<IActionResult>OnGetAsync(int editid,int deleteindex)
         {
             Mounts = await DAL.MountData.GetMounts();
+                MyUser = await _userManger.GetUserAsync(User);
 
             if (editid != 0)
             {
